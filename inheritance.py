62bb5d245e546b617bb866602410d5a3324fb57c
sudovazid <vazidmail@protonmail.com>
2022-04-05 11:55:32 +0530

    learning

---
+++ inheritance.py
@@ -0,0 +1,35 @@
+class Vehicle:
+    def general_usage(self):
+        print("general use: transporation")
+    
+class Car(Vehicle):
+    def __init__(self):
+        print("I'm car")
+        self.wheels = 4
+        self.has_roof  = True
+    
+    def specific_usage(self):
+        self.general_usage()
+        print("specific use: commute to work, vacation with family")
+
+class MotorCycle(Vehicle):
+    def __init__(self):
+        print("I'm motor cycle")
+        self.wheels = 4
+        self.has_roof = True
+
+    def specific_usage(self):
+        self.general_usage()
+        print("specific use: road trip, racing")
+    
+
+c = Car()
+
+c.specific_usage()
+
+
+m = MotorCycle()
+
+m.specific_usage()
+
+
